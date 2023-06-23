23d475f3f073b97c73e8b2bc1888862b13d6c66d
vjsubash <vjsubash79@gmail.com>
2023-06-23 08:17:18 -0700

    ShopInStyle

---
+++ src/components/Card.js
@@ -0,0 +1,47 @@
+import React,{useState} from 'react'
+
+function Card({setCartValue}){
+    let [toggle,setToggle] = useState(true)
+    return<>
+    <div className="col mb-5">
+        <div className="card h-100">
+            <div className="badge bg-dark text-white position-absolute" style={{top: "0.5rem", right: "0.5rem"}}>Sale</div>
+            <img className="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+            <div className="card-body p-4">
+                <div className="text-center">
+                    <h5 className="fw-bolder">Special Item</h5>
+                    $18.00
+                    <div class="d-flex justify-content-center small text-warning mb-2">
+                        <div class="bi-star-fill"></div>
+                        <div class="bi-star-fill"></div>
+                        <div class="bi-star-fill"></div>
+                        <div class="bi-star-fill"></div>
+                        <div class="bi-star-fill"></div>
+                    </div>
+                </div>
+            </div>
+            <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                <div className="text-center">
+                {
+                    toggle?<button className="btn btn-outline-dark mt-auto" onClick={()=>{
+                    setCartValue(prev=>prev+1)
+                    setToggle(!toggle)  
+                }}>
+                    Add to cart
+                </button>
+                :
+                <button className="btn btn-outline-dark mt-auto" onClick={()=>{
+                    setCartValue(prev=>prev-1)
+                    setToggle(!toggle)  
+                }}>
+                    Remove
+                </button>
+                }
+                </div>
+            </div>
+        </div>
+    </div>
+</>
+}
+
+export default Card
